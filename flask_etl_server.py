9bae98cf7800b16feb6339cfe6e111607fa89d6e
DungTV <dungtv@gmail.com>
2025-10-30 08:43:59 +0700

    new

--- flask_etl_server.py
+++ flask_etl_server.py
@@ -782,4 +782,5 @@ if __name__ == '__main__':
     print("⚠️  If you see any errors above, please check file locations!")
     print()
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Render sẽ inject PORT động
+    app.run(debug=False, host='0.0.0.0', port=port)
